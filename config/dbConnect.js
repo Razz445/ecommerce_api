81458cabf9a0c53a73cc0eef9bac409ce2726099
Razz445 <129313457+Razz445@users.noreply.github.com>
2023-08-07 14:09:36 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = async () => {
   try {
     // Attempt to connect to the MongoDB database
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/digitic");
+    const conn = await mongoose.connect("mongodb+srv://rajeshpanigrahi445:rajesh123@cluster0.rqyjcmx.mongodb.net/digitic");
     console.log("Database Connected Successfully");
   } catch (error) {
     // If an error occurs, log the error message
