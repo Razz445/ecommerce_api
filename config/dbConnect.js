6b056030f1a500311b5d4d35f72c0772e28ceea5
Razz445 <129313457+Razz445@users.noreply.github.com>
2023-08-07 12:34:27 +0530

    i change port 3000 to port 5000

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,14 @@
 const { default: mongoose } = require("mongoose");
 
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    const conn = mongoose.connect("mongodb://127.0.0.1:27017/digitic");
+    // Attempt to connect to the MongoDB database
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/digitic");
     console.log("Database Connected Successfully");
   } catch (error) {
-    console.log("DAtabase error");
+    // If an error occurs, log the error message
+    console.log("Database Error:", error);
   }
 };
+
 module.exports = dbConnect;
